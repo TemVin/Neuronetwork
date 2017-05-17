23f6a76db06d8b1bf677390aaa8322b9d23a1f58
Artyom Lyubeznov <LbzArt@mail.ru>
2017-05-17 20:28:58 +0300

    add graphics and fix code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,48 +6,46 @@
 #include <QVBoxLayout>
 #include <QPalette>
 #include <QMessageBox>
-#include <vector>
-
-
-std::vector<double> X0, A, G;
-double ***W;
-double **X;
-int num_neur;   // число нейронов
-int num_layers; // число слоев
-int time_n;     // время процесса
-int limit;      // ограничения на управления
-double M1;         // штрафные коэффициенты
-double M2;         // штрафные коэффициенты
-int step;       // шаг градиен. спуска
-int accuracy;   // точность
-double dt;      // dt = T/q
-
-void calc_X();
-double calc_I();
-double** calc_p();
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {   
     ui-> setupUi(this);
-    QVBoxLayout *l_tab1 = new QVBoxLayout();
 
-    QLabel *label = new QLabel();
-    QPixmap myPixmap( "C:/Qt/projects/Inna_Anatolyevna/task/task.jpg" );
-    label-> setPixmap( myPixmap );
-    l_tab1->addWidget(label);
-    ui->tabParams->setLayout(l_tab1);
+    QPixmap myPixmap("C:/Git/projects/NN/task.JPG");
+    ui->labelModel->setPixmap(myPixmap);
 
     set_defaults();
 }
-
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+void MainWindow::set_defaults()
+{
+    ui->lineEditNumNeur->setText("3");
+    ui->lineEditTime->setText("10.0");
+    ui->lineEditNumLay->setText("100");
+    ui->lineEditLimit->setText("1.0");
+    ui->lineEditAccur->setText("0.001");
+    ui->lineEditStep->setText("0.1");
+    ui->lineEditM1->setText("1.0");
+    ui->lineEditM2->setText("10.0");
+}
+
+void MainWindow::init()
+{
+    num_layers = ui->lineEditNumLay->text().toInt();
+    time_n = ui->lineEditTime->text().toDouble();
+    limit = ui->lineEditLimit->text().toDouble();
+    M1 = ui->lineEditM1->text().toDouble();
+    M2 = ui->lineEditM2->text().toDouble();
+    step = ui->lineEditStep->text().toDouble();
+    accuracy = ui->lineEditAccur->text().toDouble();
+}
 
 void MainWindow::on_lineEditNumNeur_textChanged(const QString &arg1)
 {
@@ -55,123 +53,250 @@ void MainWindow::on_lineEditNumNeur_textChanged(const QString &arg1)
 
     ui->tableWidgetInitValues->setRowCount(num_neur);
     ui->tableWidgetInitValues->setColumnCount(3);
-    ui->tableWidgetInitValues->setShowGrid(true);
+    for(int i = 0; i < num_neur; i++)
+        for(int j = 0; j < 3; j++)
+        {
+            auto item = ui->tableWidgetInitValues->item(i, j);
+            if(!item)
+            {
+                item = new QTableWidgetItem();
+                ui->tableWidgetInitValues->setItem(i, j, item);
+            }
+            item->setText("0.1");
+        }
 
     ui->tableWidgetControl->setRowCount(num_neur);
     ui->tableWidgetControl->setColumnCount(num_neur);
-    ui->tableWidgetControl->setShowGrid(true);
+    for(int i = 0; i < num_neur; i++)
+        for(int j = 0; j < num_neur; j++)
+        {
+            auto item = ui->tableWidgetControl->item(i, j);
+            if(!item)
+            {
+                item = new QTableWidgetItem();
+                ui->tableWidgetControl->setItem(i, j, item);
+            }
+            item->setText(i == j ? "0.0" : "0.5");
+        }
 }
 
 void MainWindow::on_pushButtonStart_clicked()
 {
     init();
-    dt = time_n / num_layers;
-    calc_X();
-    auto I = calc_I();
-    QMessageBox::information(this, QString("temporary view"), QString("I = %1").arg(I));
-}
+    ui->textBrowserLog->clear();
 
-void MainWindow::init()
-{
+    double *X0 = new double[num_neur];
+    double *A = new double[num_neur];
+    double *G = new double[num_neur];
     for (int i = 0; i < num_neur; i++)
     {
-        X0.push_back(ui->tableWidgetInitValues->item(i, 0)->text().toDouble());
-        A.push_back(ui->tableWidgetInitValues->item(i, 1)->text().toDouble());
-        G.push_back(ui->tableWidgetInitValues->item(i, 2)->text().toDouble());
+        X0[i] = ui->tableWidgetInitValues->item(i, 0)->text().toDouble();
+        A[i] = ui->tableWidgetInitValues->item(i, 1)->text().toDouble();
+        G[i] = ui->tableWidgetInitValues->item(i, 2)->text().toDouble();
     }
-    num_layers = ui->lineEditNumLay->text().toInt();
-    W = new double**[num_layers];
+
+    auto W = create_W();
+    for(int k = 0; k < num_layers; k++)
+        for(int i = 0; i < num_neur; i++)
+            for(int j = 0; j < num_neur; j++)
+                W[k][i][j] = ui->tableWidgetControl->item(i, j)->text().toDouble();
+
+    dt = time_n / num_layers;
+
+    auto X = calc_X(X0, G, W);
+    auto I = calc_I(A, X, W);
+
+    calc(X0, A, G, W, X, I);
+    QMessageBox::information(this, QString("temporary view"), QString("iterations done: %1").arg(iteration));
+    print(X, W);/*
+    plot(X);
+
+    destroy(W, X);
+    delete []X0;
+    delete []A;
+    delete []G;*/
+}
+
+void MainWindow::calc(double *X0, double *A, double *G, double ***W, double **X, double I)
+{
+    double ***W_, **X_, I_, dI, **p;
+    iteration = 0;
+
+    do
+    {
+        ui->textBrowserLog->append(QString("iteration %1:").arg(iteration));
+        p = calc_p(A, G, X, W);
+
+        do
+        {
+            W_ = calc_W(X, p, W);
+            X_ = calc_X(X0, G, W_);
+            I_ = calc_I(A, X_, W_);
+            ui->textBrowserLog->append((QString("I = %1").arg(I_)));
+            dI = I_ - I;
+
+            if(dI > 0.0)
+                step /= 2;
+        } while(dI > 0.0 && fabs(dI) > accuracy);
+
+        if(fabs(dI) > accuracy)
+        {
+            destroy(W, X);
+            W = std::move(W_);
+            X = std::move(X_);
+            I = I_;
+            iteration++;
+        }
+    } while(fabs(dI) > accuracy);
+}
+
+double*** MainWindow::create_W()
+{
+    double ***W = new double**[num_layers];
     for (int i = 0; i < num_layers; i++)
         W[i]= new double*[num_neur];
     for (int i = 0; i < num_layers; i++)
         for(int j = 0; j < num_neur; j++)
             W[i][j] = new double[num_neur];
-    for(int k = 0; k < num_layers; k++)
-        for(int i = 0; i < num_neur; i++)
-            for(int j = 0; j < num_neur; j++)
-                W[k][i][j] = ui->tableWidgetControl->item(i, j)->text().toDouble();
-    time_n = ui->lineEditTime->text().toInt();
-    limit = ui->lineEditLimit->text().toInt();
-    M1 = ui->lineEditM1->text().toDouble();
-    M2 = ui->lineEditM2->text().toDouble();
-    step = ui->lineEditStep->text().toInt();
-    accuracy = ui->lineEditAccur->text().toInt();
+    return W;
 }
 
-void MainWindow::set_defaults()
+void MainWindow::destroy(double ***W, double **X)
 {
-    ui->lineEditNumNeur->setText("3");
-    ui->lineEditTime->setText("10");
-    ui->lineEditNumLay->setText("300");
-    ui->lineEditLimit->setText("1");
-    ui->lineEditAccur->setText("0.01");
-    ui->lineEditStep->setText("0.1");//?
-    ui->lineEditM1->setText("1.0");
-    ui->lineEditM2->setText("1.0");
-}
-
-void calc_X()
-{
-    X = new double*[num_layers];
     for (int i = 0; i < num_layers; i++)
+        for(int j = 0; j < num_neur; j++)
+            delete []W[i][j];
+    for (int i = 0; i < num_layers + 1; i++)
+        delete []W[i];
+    delete []W;
+
+    for (int i = 0; i < num_layers + 1; i++)
+        delete []X[i];
+    delete []X;
+}
+
+double** MainWindow::calc_X(double *X0, double *G, double ***W)
+{
+    double **X = new double*[num_layers + 1];
+    for (int i = 0; i < num_layers + 1; i++)
         X[i] = new double[num_neur];
+
     for(int i = 0; i < num_neur; i++)
         X[0][i] = X0[i];
-    for(int k = 0; k < num_layers - 1; k++)
+    for(int k = 0; k < num_layers; k++)
         for(int i = 0; i < num_neur; i++)
         {
             double sum = 0.0;
             for (int j = 0; j < num_neur; j++)
                 sum += W[k][i][j] * X[k][j];
-            X[k+1][i] = X[k][i] + dt * (-G[i] * X[k][i] + sum);
+            X[k + 1][i] = X[k][i] + dt * (-G[i] * X[k][i] + sum);
         }
+    return X;
 }
 
-double calc_I()
+double MainWindow::calc_I(double *A, double **X, double ***W)
 {
     double sum1 = 0.0;
     double sum2 = 0.0;
     for(int k = 0; k < num_layers; k++)
         for(int i = 0; i < num_neur; i++)
             for(int j = 0; j < num_neur; j++)
-                sum1 += pow(W[k][i][j], 2);
+                sum1 += pow(W[k][i][j], 2.0);
     for(int i = 0; i < num_neur; i++)
-        sum2 += pow(X[num_layers - 1][i] - A[i], 2);
+        sum2 += pow(X[num_layers][i] - A[i], 2.0);
     return dt * M1 * sum1 + M2 * sum2;
 }
 
-double** calc_p()
+double** MainWindow::calc_p(double *A, double *G, double **X, double ***W)
 {
-    double **p = new double*[num_layers];
-    for(int i = 0; i < num_layers; i++)
+    double **p = new double*[num_layers + 1];
+    for(int i = 0; i < num_layers + 1; i++)
         p[i] = new double[num_neur];
+
     for(int i = 0; i < num_neur; i++)
-        p[num_layers - 1][i] = -2 * M2 * (X[num_layers - 1][i] - A[i]);
-    for(int i = 0; i < num_neur; i++)
-        for(int k = num_layers - 2; k > 0; k--)
+        p[num_layers][i] = -2 * M2 * (X[num_layers][i] - A[i]);
+    for(int k = num_layers - 1; k > 0; k--)
+        for(int i = 0; i < num_neur; i++)
         {
             double sum = 0.0;
-            for(int j = 0; j < num_neur; j++)//indeces begin with 0 or 1??????
+            for(int j = 0; j < num_neur; j++)
                 sum += W[k][i][j] * p[k + 1][j];
             p[k][i] = p[k + 1][i] - dt * G[i] * p[k + 1][i] + dt * sum;
         }
+    return p;
 }
 
-
-
-
-/*
-    QPalette pal(ui->tabWidget->palette());
-    pal.setColor(QPalette::Background, Qt::black);
-    ui->tabWidget->setPalette(pal);*/
-/*void MainWindow::on_pushButtonView_clicked()
+double*** MainWindow::calc_W(double **X, double **p, double ***W)
 {
-    this->hide();
-    QMainWindow * c = this->findChild<QMainWindow *>();
-    if (c != NULL)
+    auto W_ = create_W();
+    for(int k = 0; k < num_layers; k++)
+        for(int i = 0; i < num_neur; i++)
+            for(int j = 0; j < num_neur; j++)
+            {
+                W_[k][i][j] = W[k][i][j] - step * (2 * M1 * dt * W[k][i][j] - p[k + 1][i] * dt * X[k][j]);
+                if(W_[k][i][j] > limit)
+                    W_[k][i][j] = limit;
+                if(W_[k][i][j] < -limit)
+                    W_[k][i][j] = -limit;
+            }
+    return W_;
+}
+
+void MainWindow::print(double **X, double ***W)
+{
+    ui->tableWidgetFinalX->setRowCount(num_layers + 1);
+    ui->tableWidgetFinalX->setColumnCount(num_neur);
+    for(int i = 0; i < num_layers + 1; i++)
+        for(int j = 0; j < num_neur; j++)
+        {
+            auto item = ui->tableWidgetFinalX->item(i, j);
+            if(!item)
+            {
+                item = new QTableWidgetItem();
+                ui->tableWidgetFinalX->setItem(i, j, item);
+            }
+            item->setText(QString("%1").arg(X[i][j]));
+        }
+/*
+    for(int k = 0; k < num_layers; k++)
     {
-        c->deleteLater();
+        auto layer = new QTableWidget(this);//take existing?
+        ui->tabWidgetControl->addTab(layer, QString("Layer %1").arg(k));
+        layer->setRowCount(num_neur);
+        layer->setColumnCount(num_neur);
+        for(int i = 0; i < num_neur; i++)
+            for(int j = 0; j < num_neur; j++)
+            {
+                auto item = layer->item(i, j);
+                if(!item)
+                {
+                    item = new QTableWidgetItem();
+                    layer->setItem(i, j, item);
+                }
+                item->setText(QString("%1").arg(W[k][i][j]));
+            }
+    }*/
+}
+
+void MainWindow::plot(double **X)
+{
+    ui->customPlot->clearGraphs();
+    QVector<double> x(num_layers + 1), y(num_layers + 1);
+    for(int i = 0; i < num_layers + 1; i++)
+        x[i] = i;
+    for(int i = 0; i < num_neur; i++)
+    {
+        for(int j = 0; j < num_layers + 1; j++)
+            y[j] = X[j][i];
+        ui->customPlot->addGraph();
+        ui->customPlot->graph(i)->setData(x, y);
+        ui->customPlot->graph(i)->setPen(QPen(Qt::GlobalColor(Qt::red + i)));
     }
-    QMainWindow * v = new ViewWindow(this);
-    v->show();
-}*/
+    ui->customPlot->xAxis->setLabel("X");
+    ui->customPlot->xAxis->setRange(0.0, num_layers);
+    ui->customPlot->yAxis->setLabel("Y");
+    ui->customPlot->yAxis->setRange(-10.0, 10.0);
+    ui->customPlot->setInteraction(QCP::iRangeDrag, true);
+    ui->customPlot->setInteraction(QCP::iRangeZoom, true);
+    ui->customPlot->replot();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -16,16 +16,36 @@ public:
     ~MainWindow();
 
 private slots:
-
-
     void on_lineEditNumNeur_textChanged(const QString &arg1);
 
     void on_pushButtonStart_clicked();
 
 private:
-    Ui::MainWindow *ui;
     void init();
     void set_defaults();
+    void print(double **X, double ***W);
+    void plot(double **X);
+
+    double*** create_W();
+    void destroy(double ***W, double **X);
+    double** calc_X(double *X0, double *G, double ***W);
+    double calc_I(double *A, double **X, double ***W);
+    double** calc_p(double *A, double *G, double **X, double ***W);
+    double*** calc_W(double **X, double **p, double ***W);
+    void calc(double *X0, double *A, double *G, double ***W, double **X, double I);
+
+private:
+    Ui::MainWindow *ui;
+    int num_neur;   // число нейронов
+    int num_layers; // число слоев
+    double time_n;  // время процесса
+    double limit;   // ограничения на управления
+    double M1;      // штрафные коэффициенты
+    double M2;      // штрафные коэффициенты
+    double step;    // шаг градиен. спуска
+    double accuracy;// точность
+    double dt;      // dt = T/q
+    int iteration;  // итерация
 };
 
 #endif // MAINWINDOW_H
